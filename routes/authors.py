8728ee580b4773b865380a28befc2c69fa82d157
Pedro Moreira <moreira.plrm@gmail.com>
2026-09-05 16:40:52 -0300

    refactor: change modules names to snake_case as of Ruff N999

--- routes/authors.py
+++ routes/authors.py
@@ -2,7 +2,7 @@ from fastapi import APIRouter, HTTPException
 from sqlalchemy.orm import Session
 
 import schemas.author as AuthorSchema
-from src.controllers.v1 import ctrlsAuthor
+from src.controllers.v1 import ctrls_author
 
 from .utils import session_object, validate_request_details
 
@@ -11,20 +11,20 @@ router = APIRouter(prefix="/authors", tags=["authors"])
 
 @router.get("/")
 def getAll(db: Session = session_object):
-    authors = ctrlsAuthor.getAll(db)
+    authors = ctrls_author.getAll(db)
     return authors
 
 
 @router.get("/{author_id}")
 def getAuthor(author_id: int, db: Session = session_object):
-    author = ctrlsAuthor.get(db, author_id)
+    author = ctrls_author.get(db, author_id)
     validate_request_details(author_id, author)
     return author
 
 
 @router.post("/add")
 def authorAdd(data: AuthorSchema.AuthorPost, db: Session = session_object):
-    author = ctrlsAuthor.add(db, data.firstname, data.lastname)
+    author = ctrls_author.add(db, data.firstname, data.lastname)
     if author is None:
         raise HTTPException(status_code=409, detail="Author already exists")
     return author
@@ -34,13 +34,13 @@ def authorAdd(data: AuthorSchema.AuthorPost, db: Session = session_object):
 def authorUpdate(
     data: AuthorSchema.Author, author_id: int, db: Session = session_object
 ):
-    author = ctrlsAuthor.get(db, author_id)
+    author = ctrls_author.get(db, author_id)
     validate_request_details(author_id, author)
-    return ctrlsAuthor.patch(db, author_id, data.firstname, data.lastname)
+    return ctrls_author.patch(db, author_id, data.firstname, data.lastname)
 
 
 @router.delete("/delete/{author_id}")
 def authorDelete(author_id: int, db: Session = session_object):
-    author = ctrlsAuthor.get(db, author_id)
+    author = ctrls_author.get(db, author_id)
     validate_request_details(author_id, author)
-    return ctrlsAuthor.remove(db, author_id)
+    return ctrls_author.remove(db, author_id)
