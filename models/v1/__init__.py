687744dd1a504966e2def6b3cf3bd40a24850362
Pedro Moreira <moreira.plrm@gmail.com>
2026-09-04 22:05:55 -0300

    fix: remove hackery in __init__.py in favor of traditional imports.

--- models/v1/__init__.py
+++ models/v1/__init__.py
@@ -1,5 +1,1 @@
-from models.v1.author import Author
-from models.v1.authorBook import authorBook
-from models.v1.book import Book
-from models.v1.dec_base import Base
-from models.v1.genre import Genre
+
